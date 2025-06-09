8e896207107a306641b08afe42d23fe07bb99115
Natan <xpenatan@gmail.com>
2025-06-08 21:29:55 -0300

    Update demo version

--- demos/app/android/build.gradle.kts
+++ demos/app/android/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.github.xpenatan.webgpu.demo"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.0.3"
+        versionCode = 4
+        versionName = "0.0.4"
     }
 
     buildTypes {
