61929b71eca7ea408ed7a2ce635e98db4e5e7df8
Natan <xpenatan@gmail.com>
2025-07-14 09:55:18 -0300

    Remove custom code

--- webgpu/webgpu-base/src/main/java/com/github/xpenatan/webgpu/WGPUAdapter.java
+++
@@ -1,9 +0,0 @@
-package com.github.xpenatan.webgpu;
-
-import idl.IDLBase;
-
-public class WGPUAdapter extends IDLBase {
-    @Deprecated()
-    public WGPUAdapter(byte b, char c) {
-    }
-}
--- webgpu/webgpu-base/src/main/java/com/github/xpenatan/webgpu/WGPUDevice.java
+++
@@ -1,9 +0,0 @@
-package com.github.xpenatan.webgpu;
-
-import idl.IDLBase;
-
-public class WGPUDevice extends IDLBase {
-    @Deprecated()
-    public WGPUDevice(byte b, char c) {
-    }
-}
