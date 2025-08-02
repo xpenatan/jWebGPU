fa507b4134b28dee7a0db71b7224e4ee98a522ed
Natan <xpenatan@gmail.com>
2025-08-02 19:41:56 -0300

    Disable path when its android

--- webgpu/webgpu-base/src/main/java/com/github/xpenatan/webgpu/JWebGPULoader.java
+++ webgpu/webgpu-base/src/main/java/com/github/xpenatan/webgpu/JWebGPULoader.java
@@ -26,20 +26,35 @@ public class JWebGPULoader {
       }
     */
     public static void init(JWebGPUBackend backend, JParserLibraryLoaderListener listener) {
+        String vm = System.getProperty("java.runtime.name");
+        boolean isAndroid = vm != null && vm.contains("Android Runtime");
         String osName = System.getProperty("os.name").toLowerCase();
+
         if(!osName.contains("win")) {
             // Force WGPU if not windows. There is no libs for linux, mac or android.
             backend = JWebGPUBackend.WGPU;
         }
+        String subDir = null;
+        if(backend == JWebGPUBackend.DAWN) {
+            subDir = "native/dawn/";
+        }
+        else {
+            subDir = "native/wgpu/";
+        }
+        if(isAndroid) {
+            subDir = null; // path no supported
+        }
+        final String path = subDir;
+
         JWebGPULoader.backend = backend;
         if(backend == JWebGPUBackend.DAWN) {
             // Load dawn first and then the bindings.
             JParserLibraryLoaderOptions options = new JParserLibraryLoaderOptions();
             options.autoAddSuffix = false;
             options.autoAddPrefix = false;
-            JParserLibraryLoader.load("webgpu_dawn", "native/dawn/", options, (isSuccess, e) -> {
+            JParserLibraryLoader.load("webgpu_dawn", path, options, (isSuccess, e) -> {
                 if(isSuccess) {
-                    JParserLibraryLoader.load("jWebGPU", "native/dawn/", listener);
+                    JParserLibraryLoader.load("jWebGPU", path, listener);
                 }
                 else {
                     listener.onLoad(false, e);
@@ -48,7 +63,7 @@ public class JWebGPULoader {
         }
         else {
             // WGPU do static link so it's a single library
-            JParserLibraryLoader.load("jWebGPU", "native/wgpu/", listener);
+            JParserLibraryLoader.load("jWebGPU", path, listener);
         }
     }
 
